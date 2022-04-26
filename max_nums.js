950f52fb98bbbde311b694fcef90ec2e3b1dedbe
jackymo <jackymo@usc.edu>
2022-04-26 12:09:40 -0700

    Update max_nums.js

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,11 @@
 // start coding your function here!
+function find_max(arr){
+    let result = 0;
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] > result){
+            result = arr[i];
+        }
+    }
+    return result;
+}
+exports.find_max = find_max;
